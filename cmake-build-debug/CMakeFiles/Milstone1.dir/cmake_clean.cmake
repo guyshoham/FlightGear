364f253d60258c261bc91ab71527f9369717ab7f
Guy Shoham <guyshoham28@gmail.com>
2019-12-09 19:12:15 +0200

    init commit

---
+++ cmake-build-debug/CMakeFiles/Milstone1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Milstone1.dir/main.cpp.o"
+  "Milstone1"
+  "Milstone1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Milstone1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Milstone1.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Milstone1.
+# This may be replaced when dependencies are built.
