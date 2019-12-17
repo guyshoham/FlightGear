dd0e104648621ea280b8af0ab8c8b02b6d9dc102
Guy Shoham <guyshoham28@gmail.com>
2019-12-17 12:33:21 +0200

    implement PrintCommand execute (half)

--- Commands/PrintCommand.cpp
+++ Commands/PrintCommand.cpp
@@ -1,12 +1,24 @@
 //
 // Created by guy on 13/12/2019.
 //
-
+#include <string>
+#include <iostream>
 #include "PrintCommand.h"
+
 PrintCommand::PrintCommand() = default;
 int PrintCommand::execute(string* textArr,
                           unordered_map<string, Command*> commandTable,
                           unordered_map<string, VarInfo*> symTable) {
   //todo: implement PrintCommand::execute
+
+  string value = textArr[_index + 1];
+
+  //checks if the argument is a string or not
+  if (value.at(0) == '\"') {
+    cout << value << endl;
+  } else {
+    //todo: value is a variable or an expression. need to implement
+  }
+
   return 2;
 }
