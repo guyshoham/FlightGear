06e316828b8ec00afb463127d9e8b16c79d303f5
tom5012 <tsanderovich@gmail.com>
2019-12-23 18:19:53 +0200

    Client server that is connected to the flightGear and runs in a seperate thread

--- cmake-build-debug/CMakeFiles/Milstone1.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/Milstone1.dir/DependInfo.cmake
@@ -4,7 +4,20 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/guy/CLionProjects/Milstone1/main.cpp" "/home/guy/CLionProjects/Milstone1/cmake-build-debug/CMakeFiles/Milstone1.dir/main.cpp.o"
+  "/home/tom5012/CLionProjects/Milestone1/Commands/AssignCommand.cpp" "/home/tom5012/CLionProjects/Milestone1/cmake-build-debug/CMakeFiles/Milstone1.dir/Commands/AssignCommand.cpp.o"
+  "/home/tom5012/CLionProjects/Milestone1/Commands/Command.cpp" "/home/tom5012/CLionProjects/Milestone1/cmake-build-debug/CMakeFiles/Milstone1.dir/Commands/Command.cpp.o"
+  "/home/tom5012/CLionProjects/Milestone1/Commands/ConditionParser.cpp" "/home/tom5012/CLionProjects/Milestone1/cmake-build-debug/CMakeFiles/Milstone1.dir/Commands/ConditionParser.cpp.o"
+  "/home/tom5012/CLionProjects/Milestone1/Commands/ConnectCommand.cpp" "/home/tom5012/CLionProjects/Milestone1/cmake-build-debug/CMakeFiles/Milstone1.dir/Commands/ConnectCommand.cpp.o"
+  "/home/tom5012/CLionProjects/Milestone1/Commands/DefineVarCommand.cpp" "/home/tom5012/CLionProjects/Milestone1/cmake-build-debug/CMakeFiles/Milstone1.dir/Commands/DefineVarCommand.cpp.o"
+  "/home/tom5012/CLionProjects/Milestone1/Commands/IfCommand.cpp" "/home/tom5012/CLionProjects/Milestone1/cmake-build-debug/CMakeFiles/Milstone1.dir/Commands/IfCommand.cpp.o"
+  "/home/tom5012/CLionProjects/Milestone1/Commands/LoopCommand.cpp" "/home/tom5012/CLionProjects/Milestone1/cmake-build-debug/CMakeFiles/Milstone1.dir/Commands/LoopCommand.cpp.o"
+  "/home/tom5012/CLionProjects/Milestone1/Commands/OpenServerCommand.cpp" "/home/tom5012/CLionProjects/Milestone1/cmake-build-debug/CMakeFiles/Milstone1.dir/Commands/OpenServerCommand.cpp.o"
+  "/home/tom5012/CLionProjects/Milestone1/Commands/PrintCommand.cpp" "/home/tom5012/CLionProjects/Milestone1/cmake-build-debug/CMakeFiles/Milstone1.dir/Commands/PrintCommand.cpp.o"
+  "/home/tom5012/CLionProjects/Milestone1/Commands/SleepCommand.cpp" "/home/tom5012/CLionProjects/Milestone1/cmake-build-debug/CMakeFiles/Milstone1.dir/Commands/SleepCommand.cpp.o"
+  "/home/tom5012/CLionProjects/Milestone1/Data.cpp" "/home/tom5012/CLionProjects/Milestone1/cmake-build-debug/CMakeFiles/Milstone1.dir/Data.cpp.o"
+  "/home/tom5012/CLionProjects/Milestone1/Expressions/Calculator.cpp" "/home/tom5012/CLionProjects/Milestone1/cmake-build-debug/CMakeFiles/Milstone1.dir/Expressions/Calculator.cpp.o"
+  "/home/tom5012/CLionProjects/Milestone1/VarInfo.cpp" "/home/tom5012/CLionProjects/Milestone1/cmake-build-debug/CMakeFiles/Milstone1.dir/VarInfo.cpp.o"
+  "/home/tom5012/CLionProjects/Milestone1/main.cpp" "/home/tom5012/CLionProjects/Milestone1/cmake-build-debug/CMakeFiles/Milstone1.dir/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
